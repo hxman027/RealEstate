d70dd6d77171cd871f5d8bc5a6ea1a4f61766263
GiankDiluvi <giankdiluvi@gmail.com>
2020-04-08 15:14:50 -0700

    Update eda code. -gd

--- src/eda - s550/eda.R
+++ src/eda - s550/eda.R
@@ -7,6 +7,7 @@ library(readxl)
 library(ggplot2)
 ggplot2::theme_set(theme_classic())
 library(viridis)
+library(GGally)
 
 # data wrangling ####
 
@@ -49,6 +50,18 @@ re <- real.estate_full %>%
   dplyr::distinct() %>% 
   dplyr::left_join(assessments, by = c("PIC" = "PIC", "year" = "year")) # add assessment
 
+# properties by municipality 2020
+re %>% 
+  dplyr::select(municipality, PIC) %>% 
+  #dplyr::filter(year == 2020) %>% 
+  dplyr::group_by(municipality) %>% 
+  dplyr::summarise(n.properties = n()) %>% 
+  dplyr::arrange(desc(n.properties)) %>% 
+  dplyr::mutate(perc.properties = n.properties / sum(n.properties),
+                acum.properties = cumsum(perc.properties)) %>% 
+  View()
+  
+  
 
 # data viz ####
 # tax classes dictionary
@@ -68,10 +81,11 @@ re %>%
   ggplot(aes(x = log(total.assessment), y = log(mill.rate), color = factor(year))) +
   geom_point() +
   facet_wrap(.~tax.class, labeller = tax.classes) +
-  labs(x = "log assessment",
-       y = "log mill rate",
+  labs(x = "mean log assessment value",
+       y = "mean log mill rate",
        color = "year") +
-  scale_color_viridis_d()
+  scale_color_viridis_d() +
+  theme(text = element_text(size = 18))
 ggsave("RealEstate/src/eda - s550/plots/1. scatter with year.pdf")
 ggsave("RealEstate/src/eda - s550/plots/1. scatter with year.png")
 
@@ -86,9 +100,9 @@ re %>%
   ggplot(aes(x = log(total.assessment), y = log(mill.rate))) +
   geom_point(color = viridis(20)[3]) +
   facet_wrap(.~tax.class, labeller = tax.classes) +
-  labs(x = "log assessment",
-       y = "log mill rate",
-       title = "Assessment vs mill rates by municipality for 2020")
+  labs(x = "mean log assessment value",
+       y = "mean log mill rate") +
+  theme(text = element_text(size = 18))
 ggsave("RealEstate/src/eda - s550/plots/2. scatter 2020 by municipality.pdf")
 ggsave("RealEstate/src/eda - s550/plots/2. scatter 2020 by municipality.png")
 
@@ -175,10 +189,33 @@ ggsave("RealEstate/src/eda - s550/plots/4. scatter average year.png")
     #scale_color_viridis_d() +
     #theme(legend.position = "none") +
     labs(x = "year",
-         y = "log mill rate",
-         title = "Mill rates over time for some municipalities")
+         y = "log mill rate") +
+    theme(text = element_text(size = 18),
+          axis.text.x = element_text(angle = 45, hjust = 1))
   ggsave("RealEstate/src/eda - s550/plots/6. mill rate evolution sample.pdf")
   ggsave("RealEstate/src/eda - s550/plots/6. mill rate evolution sample.png")
+  
+  
+  # facet line trends sample of 10 assessment
+  re %>% 
+    dplyr::select(-PIC) %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate)) %>% 
+    dplyr::group_by(year, tax.class) %>% 
+    dplyr::sample_n(size = 10) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::group_by(year, tax.class, municipality) %>% 
+    dplyr::summarise(total.assessment = mean(total.assessment), mill.rate = mean(mill.rate)) %>% 
+    ggplot(aes(x = year, y = log(total.assessment), group = municipality)) +
+    geom_line(color = viridis(20)[3]) +
+    facet_wrap(.~tax.class, labeller = tax.classes) + 
+    #scale_color_viridis_d() +
+    #theme(legend.position = "none") +
+    labs(x = "year",
+         y = "log assessment value") +
+    theme(text = element_text(size = 18),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  ggsave("RealEstate/src/eda - s550/plots/6.1 assessment evolution sample.pdf")
+  ggsave("RealEstate/src/eda - s550/plots/6.1 assessment evolution sample.png")
   
   
   # violin plots of mill rates accross tax classes, for 2020
@@ -190,13 +227,33 @@ ggsave("RealEstate/src/eda - s550/plots/4. scatter average year.png")
     geom_violin(alpha = 0.5, width = 1) +
     geom_boxplot(alpha = 0.75, width = 0.1) +
     labs(x = "tax class",
-         y = "log mill rate",
-         title = "Mill rates accross tax classes") +
+         y = "log mill rate") +
     #scale_fill_viridis_d(begin=0, end=1) +
     scale_fill_manual(values = c("#3E4A89FF", "#26828EFF","#B4DE2CFF" )) +
-    theme(legend.position = "none")
+    theme(legend.position = "none") +
+    scale_x_discrete(labels = c("01 - Residential", "05 - Industrial", "06 - Commercial")) +
+    theme(text = element_text(size = 18))
   ggsave("RealEstate/src/eda - s550/plots/7. violin mill rates.pdf")
   ggsave("RealEstate/src/eda - s550/plots/7. violin mill rates.png")
+  
+  
+  # violin plots of assessment values accross tax classes, for 2020
+  re %>%  
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020) %>% 
+    dplyr::group_by(PIC, tax.class) %>% 
+    dplyr::summarise(total.assessment = mean(total.assessment), mill.rate = mean(mill.rate)) %>% 
+    ggplot(aes(x = tax.class, y = log(total.assessment), fill = tax.class)) +
+    geom_violin(alpha = 0.5, width = 1) +
+    geom_boxplot(alpha = 0.75, width = 0.1) +
+    labs(x = "tax class",
+         y = "log assessment value") +
+    #scale_fill_viridis_d(begin=0, end=1) +
+    scale_fill_manual(values = c("#3E4A89FF", "#26828EFF","#B4DE2CFF" )) +
+    theme(legend.position = "none") +
+    scale_x_discrete(labels = c("01 - Residential", "05 - Industrial", "06 - Commercial")) +
+    theme(text = element_text(size = 18))
+  ggsave("RealEstate/src/eda - s550/plots/7.1 violin assessment values.pdf")
+  ggsave("RealEstate/src/eda - s550/plots/7.1 violin assessment values.png")
   
   
   # boxplots of mill rates accross tax classes, for 2020
@@ -212,7 +269,86 @@ ggsave("RealEstate/src/eda - s550/plots/4. scatter average year.png")
   ggsave("RealEstate/src/eda - s550/plots/8. boxplot mill rates.pdf")
   ggsave("RealEstate/src/eda - s550/plots/8. boxplot mill rates.png")
   
-# export data set
+  
+
+# correlation plot
+  re %>% 
+    dplyr::select(-PIC) %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020) %>% 
+    dplyr::select(municipality, total.assessment) %>% 
+    dplyr::group_by(municipality) %>% 
+    dplyr::summarise(total.assessment = mean(total.assessment)) %>% 
+    tidyr::spread(key = municipality, value = total.assessment) %>% 
+    cor() %>% 
+    corrplot::corrplot(method = "color",
+                       type = "upper",
+                       tl.col = "black",
+                       tl.pos="n")
+  
+  
+  re %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020, tax.class == "01") %>% 
+    dplyr::select(PIC, total.assessment) %>% 
+    dplyr::group_by(PIC) %>% 
+    dplyr::summarise(total.assessment = mean(total.assessment)) %>% 
+    tidyr::spread(key = PIC, value = total.assessment) %>% 
+    cor() %>% 
+    corrplot::corrplot(method = "color",
+                       type = "upper",
+                       tl.col = "black",
+                       tl.pos="n")
+  
+  
+  re %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020, tax.class == "01") %>% 
+    dplyr::select(municipality, total.assessment) %>% 
+    GGally::ggpairs(cardinality_threshold = 162)
+  
+  re %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020, tax.class == "01") %>% 
+    dplyr::select(total.assessment) %>% 
+    acf(plot = FALSE) %>% 
+    with(data.frame(lag, acf)) %>% 
+    ggplot(aes(lag, acf)) +
+    geom_hline(aes(yintercept = 0)) +
+    geom_segment(mapping = aes(xend = lag, yend = 0)) +
+    geom_hline(aes(yintercept = 1.96/sqrt(nrow(re))),
+               linetype = 2, color = 'darkblue') + 
+    geom_hline(aes(yintercept = -1.96/sqrt(nrow(re))),
+               linetype = 2, color = 'darkblue') +
+    labs(x = "Lag",
+         y = "Autocorrelation function") +
+    theme(text = element_text(size = 18))
+  
+  re %>% 
+    dplyr::filter(!is.na(total.assessment), !is.na(mill.rate), year == 2020, tax.class == "01") %>% 
+    dplyr::select(municipality, total.assessment) %>% 
+    dplyr::group_by(municipality) %>% 
+    dplyr::summarise(total.assessment = mean(total.assessment)) %>%  
+    dplyr::select(total.assessment) %>% 
+    acf(plot = FALSE) %>% 
+    with(data.frame(lag, acf)) %>% 
+    ggplot(aes(lag, acf)) +
+    geom_hline(aes(yintercept = 0)) +
+    geom_segment(mapping = aes(xend = lag, yend = 0)) +
+    geom_hline(aes(yintercept = 1.96/sqrt(nrow(re))),
+               linetype = 2, color = 'darkblue') + 
+    geom_hline(aes(yintercept = -1.96/sqrt(nrow(re))),
+               linetype = 2, color = 'darkblue') +
+    labs(x = "Lag",
+         y = "Autocorrelation function") +
+    theme(text = element_text(size = 18))
+  
+  
+  
+  
+corr <- data.frame(assessment = corr.values$total.assessment)
+rownames(corr) <- corr.values$municipality
+cor(corr)
+  ggsave("RealEstate/src/eda - s550/plots/9. correlation matrix.pdf")
+  ggsave("RealEstate/src/eda - s550/plots/9. correlation matrix.png")
+
+# export data set ####
 set.seed(2303)
 train <- re %>% 
   dplyr::select(PIC) %>% 
